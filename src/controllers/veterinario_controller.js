0486547e68df2a8decf932991a948f0ccf911e40
Ruizerick26 <ruizerick2525@gmail.com>
2024-01-08 16:38:47 -0500

    Entorno y registro

---
+++ src/controllers/veterinario_controller.js
@@ -0,0 +1,58 @@
+import Veterinario from "../models/veterinario.js"
+
+
+const login =(req,res)=>{
+    res.status(200).json({res:'login del veterinario'})
+}
+const perfil=(req,res)=>{
+    res.status(200).json({res:'perfil del veterinario'})
+}
+const registro = async (req,res)=>{
+    const {email,password} = req.body
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    const nuevoVeterinario = new Veterinario(req.body)
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    nuevoVeterinario.crearToken()
+    await nuevoVeterinario.save()
+    res.status(200).json({nuevoVeterinario})
+}
+const confirmEmail = (req,res)=>{
+    res.status(200).json({res:'confirmar email de registro de veterinario'})
+}
+const listarVeterinarios = (req,res)=>{
+    res.status(200).json({res:'lista de veterinarios registrados'})
+}
+const detalleVeterinario = (req,res)=>{
+    res.status(200).json({res:'detalle de un veterinario registrado'})
+}
+const actualizarPerfil = (req,res)=>{
+    res.status(200).json({res:'actualizar perfil de un veterinario registrado'})
+}
+const actualizarPassword = (req,res)=>{
+    res.status(200).json({res:'actualizar password de un veterinario registrado'})
+}
+const recuperarPassword= (req,res)=>{
+    res.status(200).json({res:'enviar mail recuperación'})
+}
+const comprobarTokenPasword= (req,res)=>{
+    res.status(200).json({res:'verificar token mail'})
+}
+const nuevoPassword= (req,res)=>{
+    res.status(200).json({res:'crear nuevo password'})
+}
+
+export {
+    login,
+    perfil,
+    registro,
+    confirmEmail,
+    listarVeterinarios,
+    detalleVeterinario,
+    actualizarPerfil,
+    actualizarPassword,
+	recuperarPassword,
+    comprobarTokenPasword,
+	nuevoPassword
+}
