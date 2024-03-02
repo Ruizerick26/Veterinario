713d8fbc8e75687b5c68d9f6689c73d8b2af8bc0
Ruizerick26 <ruizerick2525@gmail.com>
2024-03-01 21:57:21 -0500

    "full"

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ connection()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: process.env.URL_FRONTEND
     }
 })
 
